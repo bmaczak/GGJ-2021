5e9b7e9f447e6e198f033b069be8e73510d933d1
bmaczak <maczak.balazs@gmail.com>
2021-01-30 21:57:51 +0100

    Rotating level only under a velocity threshold

--- Assets/Scripts/RotationManager.cs
+++ Assets/Scripts/RotationManager.cs
@@ -37,13 +37,16 @@ public class RotationManager : MonoBehaviour
 
 	private void CheckPlaneRotation()
 	{
+		Debug.Log(Mathf.Abs(_player.GetComponent<Rigidbody>().velocity.x));
 		if (Mathf.Abs(_player.transform.position.x - (_playerRadius + _wallWidth)) < 0.01f 
-			&& _player.InputVector.x == -1)
+			&& _player.InputVector.x == -1
+			&& Mathf.Abs(_player.GetComponent<Rigidbody>().velocity.x) < 0.0001f)
 		{
 			_mazeRotator.RotateBy(Quaternion.AngleAxis(-120, new Vector3(1, 1, -1)));
 		}
 		else if (Mathf.Abs(_player.transform.position.z + (_playerRadius + _wallWidth)) < 0.01f 
-			&& _player.InputVector.z == 1)
+			&& _player.InputVector.z == 1
+			&& Mathf.Abs(_player.GetComponent<Rigidbody>().velocity.z) < 0.0001f)
 		{
 			_mazeRotator.RotateBy(Quaternion.AngleAxis(120, new Vector3(1, 1, -1)));
 		}
